6f7a0f05bd482591704c88da52ab317af6a809a1
ardee222 <ardeeche@gmail.com>
2022-12-30 20:41:42 +0800

    Update App.svelte

--- my-app/src/App.svelte
+++ my-app/src/App.svelte
@@ -94,8 +94,8 @@
     </svg>
     <input class="bg-gray-500 p-2 focus:bg-white focus:border-b-4 border-blue-100" type="text" placeholder="搜尋郵件">
   </form>
-  <div class="md:translate-y-12 max-w-md w-screen max-h-[500px] h-full rounded-xl bg-white mb-2 pt-2">
-    <div class="flex place-content-between mb-2 mx-4 sticky top-0">
+  <div class="md:translate-y-12 max-w-md w-screen rounded-xl bg-white mb-2 pt-2 overflow-hidden">
+    <div class="flex place-content-between mb-2 mx-4 ">
       <div class="btn hover:bg-gray-200 duration p-2 rounded-full">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
           <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
@@ -114,8 +114,13 @@
         </div>
       </div>
     </div>
-    <hr class="sticky top-10">
-    <div class="flex flex-col h-96 my-2 justify-center items-center overflow-y-scroll">
+    <hr>
+    <div class="flex flex-col max-h-[500px] lg:max-h-[450px] h-full my-2 items-center overflow-y-scroll">
+      <div>1暫時冇信息...</div>
+      <div>2暫時冇信息...</div>
+      <div>3暫時冇信息...</div>
+      <div>4暫時冇信息...</div>
+      <div>5暫時冇信息...</div>
       <div>暫時冇信息...</div>
       <div>暫時冇信息...</div>
       <div>暫時冇信息...</div>
@@ -148,12 +153,7 @@
       <div>暫時冇信息...</div>
       <div>暫時冇信息...</div>
       <div>暫時冇信息...</div>
-      <div>暫時冇信息...</div>
-      <div>暫時冇信息...</div>
-      <div>暫時冇信息...</div>
-      <div>暫時冇信息...</div>
-      <div>暫時冇信息...</div>
-      <div>暫時冇信息...</div>
+      <div>last暫時冇信息...</div>
     </div>
     <div class="text-xs text-gray-400 mx-4">
       <div>你已使用xxGB</div>
@@ -163,7 +163,7 @@
   {/if}
   <!--writing mail-->
   {#if current_index === 0}
-  <div class="relative max-w-md w-screen max-h-[500px] h-full rounded-xl space-y-2 bg-white mb-2 p-4">
+  <div class="relative max-w-md w-screen max-h-[600px] lg:max-h-[500px] h-full rounded-xl space-y-2 bg-white mb-2 p-4">
     <form class="flex focus-within:text-blue-200">
       <div class="shrink-0">收件人(編號):</div>
       <input type="text">
