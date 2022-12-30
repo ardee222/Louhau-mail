df3213aaa05f6c7f9512a84d4eb64b5310727442
ardee222 <ardeeche@gmail.com>
2022-12-30 21:09:16 +0800

    Update App.svelte

--- my-app/src/App.svelte
+++ my-app/src/App.svelte
@@ -87,7 +87,7 @@
   </div>
 
   <!--all mail-->
-  {#if current_index === 1}
+  {#if current_index === 1 || current_index === 2}
   <form class="flex items-center space-x-2 pl-2 md:visible invisible max-w-sm w-screen absolute rounded-lg bg-gray-500 focus-within:bg-white focus:border " action="">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
       <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -96,21 +96,30 @@
   </form>
   <div class="md:translate-y-12 max-w-md w-screen rounded-xl bg-white mb-2 pt-2 overflow-hidden">
     <div class="flex place-content-between mb-2 mx-4 ">
-      <div class="btn hover:bg-gray-200 duration p-2 rounded-full">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-        </svg>
-      </div>
-      <div class="flex space-x-2">
+      <div class="relative">
         <div class="btn hover:bg-gray-200 duration p-2 rounded-full">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
           </svg>
         </div>
-        <div class="btn hover:bg-gray-200 duration p-2 rounded-full">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
+        <div class="hide">刷新</div>
+      </div>
+      <div class="flex space-x-2">
+        <div class="relative">
+          <div class="btn hover:bg-gray-200 duration p-2 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+            </svg>
+          </div>
+          <div class="hide">上一頁</div>
+        </div>
+        <div class="relative">
+          <div class="btn hover:bg-gray-200 duration p-2 rounded-full">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+            </svg>
+          </div>
+          <div class="hide">下一頁</div>
         </div>
       </div>
     </div>
@@ -172,16 +181,27 @@
     <input type="text" placeholder="主旨">
     <div class="w-sm border-b"></div>
     <input type="text" placeholder="輸入正文" class="w-sm overflow-auto">
-    <div class="absolute bottom-2 flex space-x-2 items-center">
-      <button type="submit" class="text-xs bg-green-100 active:text-yellow-300">傳送</button>
-      <div class="hover:bg-gray-300 p-2 rounded-full">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776" />
-        </svg>
+    <div class="absolute bottom-2 flex items-center space-x-2">
+      <button type="submit" class="text-sm bg-green-100 p-4 active:text-yellow-300">傳送</button>
+      <div class="relative">
+        <div class="btn hover:bg-gray-300 p-2 rounded-full">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776" />
+          </svg>    
+        </div>
+        <div class="hide">附件</div>
       </div>
-      
+      <div class="relative">
+        <div class="btn hover:bg-gray-300 p-2 rounded-full">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+          </svg>
+        </div>
+        <div class="hide ">查看學生/教師編號</div>
+      </div>
     </div>
   </div>
+
   <!--function list on phone-->
   {/if}
   <div class="flex md:visible lg:invisible bg-green-100">
@@ -242,4 +262,15 @@ input:focus{
 .btn:active {
     transform: translateY(-10px);
   }
+
+.hide{
+  position: fixed;
+  display: none;
+  transform: translateY(10px);
+}
+
+.btn:hover + .hide{
+  display: block;
+  color:black
+}
 </style>
