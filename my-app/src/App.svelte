fad783a395a13a286d8ddd3f03336b13cce7955b
ardee222 <ardeeche@gmail.com>
2022-12-30 19:04:09 +0800

    Update App.svelte

--- my-app/src/App.svelte
+++ my-app/src/App.svelte
@@ -13,8 +13,10 @@
     6:'六',
     7:'日'
     }
-  let current_index = 1;
+  let current_index;
+  $: current_index = 1;
   let opening_manu = false;
+
 </script>
 
 <main class="max-w-md w-screen h-screen">
@@ -33,7 +35,7 @@
     </div>
     <div class="flex my-4 p-4 bg-gray-400 hover:bg-gray-500 rounded-lg max-w-[200px] w-full drop-shadow-lg"
     class:bg-gray-500 = "{current_index === 0}"
-    on:mouseup={()=> {current_index = 0}}
+    on:mouseup={()=> {current_index = 0;}}
     >
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
@@ -84,14 +86,15 @@
     </div>
   </div>
 
-  <!--收件匣-->
+  <!--all mail-->
+  {#if current_index === 1}
   <form class="flex items-center space-x-2 pl-2 md:visible invisible max-w-sm w-screen absolute rounded-lg bg-gray-500 focus-within:bg-white focus:border " action="">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
       <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
     </svg>
     <input class="bg-gray-500 p-2 focus:bg-white focus:border-b-4 border-blue-100" type="text" placeholder="搜尋郵件">
   </form>
-  <div class="md:translate-y-12 max-w-md w-screen max-h-[500px] h-full rounded-xl bg-white mb-10 pt-2">
+  <div class="md:translate-y-12 max-w-md w-screen max-h-[500px] h-full rounded-xl bg-white mb-2 pt-2">
     <div class="flex place-content-between mb-2 mx-4 sticky top-0">
       <div class="btn hover:bg-gray-200 duration p-2 rounded-full">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
@@ -157,20 +160,67 @@
       <div>剩返yyGB可用</div>
     </div>
   </div>
-  <!--function list on phone-->
-  <div class="flex md:visible lg:invisible bg-green-100">
-    <div class="w-1/4 border border-white flex py-3 justify-center items-center">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="orange" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-      </svg>
+  {/if}
+  <!--writing mail-->
+  {#if current_index === 0}
+  <div class="relative max-w-md w-screen max-h-[500px] h-full rounded-xl space-y-2 bg-white mb-2 p-4">
+    <form class="flex focus-within:text-blue-200">
+      <div>收件人(編號):</div>
+      <input type="text">
+    </form>
+    <div class="w-sm border-b"></div>
+    <input type="text" placeholder="主旨">
+    <div class="w-sm border-b"></div>
+    <input type="text" placeholder="輸入正文" class="w-sm overflow-auto">
+    <div class="absolute bottom-2 flex space-x-2 items-center">
+      <button type="submit" class="text-xs bg-green-100 active:text-yellow-300">傳送</button>
+      <div class="hover:bg-gray-300 p-2 rounded-full">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776" />
+        </svg>
+      </div>
+      
     </div>
-    <div class="w-1/4 border border-white flex py-3 justify-center items-center">
+  </div>
+  <!--function list on phone-->
+  {/if}
+  <div class="flex md:visible lg:invisible bg-green-100">
+    <div class="w-1/4 flex py-3 justify-center items-center"
+    on:mouseup={()=> {current_index = 0}}
+    >
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="orange" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
       </svg>
     </div>
-    <div class="w-1/4 border border-white flex py-3 justify-center items-center"></div>
-    <div class="w-1/4 border border-white flex py-3 justify-center items-center"></div>
+    <div class="w-1/4 flex py-3 justify-center items-center"
+    on:mouseup={()=> {current_index = 1}}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="orange" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+      </svg>  
+    </div>
+    <div class="w-1/4 flex py-3 justify-center items-center"
+    on:mouseup={()=> {current_index = 2}}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="orange" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+      </svg>
+    </div>
+    <div class="w-1/4 flex py-3 justify-center items-center"
+    on:mouseup={()=> {current_index = 3}}
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="orange" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+      </svg>
+    </div>
+  </div>
+  <div class="md:visible lg:invisible border-4 border-white rounded-lg bg-white w-full h-5">
+    <div class="w-1/5 h-3 bg-green-300 rounded-lg duration-100"
+    class:translate-x-[0%]="{current_index===1}"
+    class:translate-x-[130%]="{current_index===1}"
+    class:translate-x-[260%]="{current_index===2}"
+    class:translate-x-[390%]="{current_index===3}"
+    ></div>
   </div>
 </main>
 
