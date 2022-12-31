ed04e3d04cdada6b2c575e6fb789fee7bc10e684
ardee222 <ardeeche@gmail.com>
2022-12-31 12:28:31 +0800

    Update App.svelte

--- my-app/src/App.svelte
+++ my-app/src/App.svelte
@@ -97,7 +97,9 @@
   <div class="md:translate-y-12 max-w-md w-screen rounded-xl bg-white mb-2 pt-2 overflow-hidden">
     <div class="flex place-content-between mb-2 mx-4 ">
       <div class="relative">
-        <div class="btn hover:bg-gray-200 duration p-2 rounded-full">
+        <div class="btn hover:bg-gray-200 duration p-2 rounded-full"
+        on:mouseup={() => {location.reload()}}
+        >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
           </svg>
