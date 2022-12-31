a53d901c3cd9a15aaa63b0c72b474f977a078a81
ardee222 <ardeeche@gmail.com>
2022-12-31 17:54:55 +0800

    cancel prototype css

--- my-app/src/App.svelte
+++ my-app/src/App.svelte
@@ -41,7 +41,7 @@
         <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
       </svg>
       {#if !opening_manu}
-      <div class="px-4">撰寫</div>  
+      <div class="px-4 text-white">撰寫</div>  
       {/if}
     </div>
     <div class="flex mt-2 p-4 hover:bg-green-100 rounded-lg"
@@ -52,7 +52,7 @@
         <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
       </svg>      
       {#if !opening_manu}
-      <a href="https://www.louhau.edu.mo/" class="mb-2 mx-6 text-black">收件匣</a>
+      <a class="mb-2 mx-6 text-black">收件匣</a>
       {/if}
     </div>
     <div class="flex mt-2 p-4 hover:bg-green-100 rounded-lg"
@@ -63,7 +63,7 @@
         <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
       </svg>
       {#if !opening_manu}
-      <a href="https://www.louhau.edu.mo/" class="mb-2 mx-6 text-black">新信息</a>
+      <a class="mb-2 mx-6 text-black">新信息</a>
       {/if}
     </div>
     <div class="flex mt-2 p-4 hover:bg-green-100 rounded-lg"
