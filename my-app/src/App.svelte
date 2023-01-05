01c03e7368a179b110ddc029d9d03881e43176ab
ardee222 <ardeeche@gmail.com>
2023-01-05 17:54:27 +0800

    Update App.svelte

--- my-app/src/App.svelte
+++ my-app/src/App.svelte
@@ -219,6 +219,16 @@
   </div>
   {/if}
 
+  <!--back to home page-->
+  {#if current_index === 3}
+  <div class="flex">
+    <div>loading</div>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+    </svg>    
+  </div>
+  {/if}
+
   <!--function list on phone-->
   <div class="flex md:visible lg:invisible bg-green-100">
     <div class="w-1/4 flex py-3 justify-center items-center"
@@ -289,4 +299,5 @@ input:focus{
   display: block;
   color:black
 }
+
 </style>
