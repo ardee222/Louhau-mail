9836f39c4785cd18ee7ffc9620d49ea6967242ea
ardee222 <ardeeche@gmail.com>
2023-01-25 18:01:43 +0800

    Update App.svelte

--- my-app/src/App.svelte
+++ my-app/src/App.svelte
@@ -24,12 +24,12 @@
   $: current_index = 1;
   let opening_manu = false;
   let showing_data = false;
-  $: rotate_deg = 30;
+  $: rotate_deg = 0;
   function loading(){
     setInterval(() => {
-      rotate_deg += 30
-      document.getElementById('rotate').style = `transform:rotate(${rotate_deg});`
-    }, 500);
+      rotate_deg += 1
+      document.getElementById('rotate').style = `transform:rotate(${rotate_deg}deg);`
+    }, 10);
   }
   
 </script>
@@ -265,11 +265,9 @@
   {#if current_index === 3}
   <div class="flex inset-0 space-x-2 bg-gray-400 absolute h-screen w-screen flex justify-center items-center">
     <div class="text-xl">loading</div>
-    <div id="rotate">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" id="rotate">
         <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-      </svg>
-    </div>    
+      </svg>   
   </div>
   {/if}
 
